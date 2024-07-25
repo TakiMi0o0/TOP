5fc7ef1eb7cf0eee21bb8894aaeb5e67084d7c47
TakiMi0o0 <takimiworks@gmail.com>
2024-07-25 16:24:53 +0900

    Initial commit

---
+++ contact/task8-2.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>完了ページ | HP</title>
+  <meta name="direction" content="">
+  <link rel="stylesheet" href="/reset.css">
+  <link rel="stylesheet" href="stylesheet.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20,400,0,0" />
+</head>
+
+<body>
+  <!--  ヘッダー&メインイメージ  -->
+  <header>
+    <div class="header-main">
+      <h1 class="header-left">ここには会社名が入ります</h1>
+      <div class="header-right">
+        <a href="#"><div class="left-btn">ボタン01</div></a>
+        <a href="#"><div class="right-btn">ボタン02</div></a>
+      </div>
+    </div>
+    <ul class="header-menu">
+    <li class="menu01"><a href="/PHP/">Tasks</a></li>
+      <li class="menu02"><a href="#">メニュー02</a></li>
+      <li class="menu02"><a href="#">メニュー03</a></li>
+    </ul>
+  </header>
+  <div class="main-image">
+    <img src="mv.png" alt="">
+  </div>
+  <div class="contact">
+    <div class="contact-text">
+      <h2 class="contact-text-title">お問い合わせ</h2>
+      <h2 class="form_sent">送信完了しました。</h2>
+    </div>
+  </div>
+
+  <!--  リンク  -->
+  <div class="links">
+    <div class="link-left">
+      <div class="link_text">こちらからご購入ください</div>
+      <a href="#"><div class="left-btn">ネットショップ</div></a>
+    </div>
+    <div class="link-right">
+      <div class="link_text">お気軽にお問い合わせください</div>
+      <a href="#"><div class="right-btn">お問い合わせ</div></a>
+    </div>
+  </div>
+
+  <!--  フッター  -->
+  <footer>
+    <div class="company-prof">
+      <div class="company-name">ここには会社名が入ります</div>
+      <div class="company-address">住所が入ります</div>
+      <div class="company-phone">03-1234-5678</div>
+      <div class="company-time">営業時間：9:00～18:00</div>
+    </div>
+    <div class="footer-menu">
+      <ul class="footer-links">
+        <li class="footer-link01"><a href="#">リンク01</a></li>
+        <li class="footer-link02"><a href="#">リンク02</a></li>
+        <li class="footer-link03"><a href="#">リンク03</a></li>
+        <li class="footer-link04"><a href="#">リンク04</a></li>
+        <li class="footer-link05"><a href="#">リンク05</a></li>
+        <li class="footer-link06"><a href="#">リンク06</a></li>
+      </ul>
+      <ul class="footer-links">
+        <li class="footer-link07"><a href="#">リンク07</a></li>
+      </ul>
+    </div>
+    <p class="footer-text">ここには会社名が入ります©Copyright.</p>
+  </footer>
+</body>
+</html>
